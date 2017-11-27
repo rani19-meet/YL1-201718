91370c24d95061661774f2003e33dcf3362670a7
shellyw19-meet <shellyw19@meet.mit.edu>
2017-11-27 19:02:07 +0200

    lab5

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -2,6 +2,25 @@ from turtle import Turtle, colormode
 import turtle
 import random
 colormode(255)
+class Hexagon(Turtle):
+    def __init__(self,size):
+        Turtle.__init__(self)
+        self.shapesize(size)
+turtle.begin_poly()
+turtle.forward(90)
+turtle.right(50)
+turtle.forward(90)
+tortle.right(50)
+turtle.forward(90)
+turtle.right(50)
+turtle.forward(90)
+turtle.right(50)
+turtle.forward(90)
+turtle.right(50)
+turtle.forward(90)
+turtle.end_poly()
+p=turtle.get_poly()
+register_shape("myFavouriteShape",p)
 class Square(Turtle):
     def __init__(self,size):
         Turtle.__init__(self)
@@ -12,8 +31,7 @@ class Square(Turtle):
         g=random.randint(0,256)
         b=random.randint(0,256)
         self.color(r,g,b)
-    
-    
+hexagon1=Hexagon(30)
 square1=Square(20)
 square1.random_color()
 turtle.mainloop()
